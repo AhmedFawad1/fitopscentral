b31588d47a2713124c25d6abc43749832dd8433d
unknown <ahmed.fawadmajoka@gmail.com>
2026-03-03 13:11:16 +0500

    updating web app

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const isTauri = process.env.BUILD_TARGET === 'tauri';
+const cspHeader = `default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; connect-src 'self' ws:; frame-src 'none';`;
+
 const nextConfig = {
   output: isTauri ? 'export' : undefined,
   images: { unoptimized: true },
   trailingSlash: true,
   reactStrictMode: false,
-  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
